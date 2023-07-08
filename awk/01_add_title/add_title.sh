a6ee82e0ca13aef18e00a80734fe3e5f16009b3b
Figo.Xu <xujh945@qq.com>
2023-07-08 10:18:22 +0800

    update sh

--- awk/01_add_title/add_title.sh
+++ awk/01_add_title/add_title.sh
@@ -4,8 +4,13 @@ function print_title() {
   awk 'BEGIN{printf "Sr No\tName\tSub\tMarks\n"} {print}' marks.txt
 }
 
+function print_title_by_awk_file() {
+    awk -f add_title.awk marks.txt
+}
+
 function main() {
     print_title
+    print_title_by_awk_file
 }
 
 main
